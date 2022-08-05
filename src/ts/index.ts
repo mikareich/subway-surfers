ad115ff36a440525677840a9abe56132066e4865
Mika Reich <mikareich0@gmail.com>
2022-08-05 23:06:44 +0200

    embed game canvas in typescript

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,7 @@
+import { GAME_WIDTH } from "./constants";
+
+const gameScreen = <HTMLCanvasElement>document.getElementById("gameScreen");
+gameScreen.width = GAME_WIDTH;
+gameScreen.height = GAME_HEIGHT;
+
 export {};
