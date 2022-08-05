94d49010dd0106dc87f0bc2f1ca3390a68b5a541
Mika Reich <mikareich0@gmail.com>
2022-08-05 23:22:06 +0200

    add simple render engine

---
+++ src/ts/Renderer/Renderer.ts
@@ -0,0 +1,43 @@
+import { GAME_WIDTH, GAME_HEIGHT } from "../constants";
+import { Position } from "../interfaces";
+import RenderObject from "./RenderObject";
+
+export interface RenderData {
+  width: number;
+  height: number;
+  position: Position;
+  backgroundColor: string;
+}
+
+class Renderer {
+  static render(renderData: RenderData, context: CanvasRenderingContext2D) {
+    const { width, height, position, backgroundColor } = renderData;
+    const { x, y } = position;
+
+    context.fillStyle = backgroundColor;
+    context.fillRect(x, y, width, height);
+  }
+
+  public readonly gameScreen: HTMLCanvasElement;
+
+  public readonly context: CanvasRenderingContext2D;
+
+  public readonly renderObjects: RenderObject[] = [];
+
+  constructor(gameScreen: HTMLCanvasElement) {
+    this.gameScreen = gameScreen;
+    this.context = gameScreen.getContext("2d")!;
+
+    // udpate the game screen size
+    this.gameScreen.width = GAME_WIDTH;
+    this.gameScreen.height = GAME_HEIGHT;
+  }
+
+  renderAll() {
+    this.renderObjects.forEach((renderObject) => {
+      Renderer.render(renderObject.renderData, this.context);
+    });
+  }
+}
+
+export default Renderer;
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,7 +1,11 @@
 import { GAME_HEIGHT, GAME_WIDTH } from "./constants";
+import Renderer, { RenderObject } from "./Renderer";
 
 const gameScreen = <HTMLCanvasElement>document.getElementById("gameScreen");
-gameScreen.width = GAME_WIDTH;
-gameScreen.height = GAME_HEIGHT;
 
-export {};
+const renderer = new Renderer(gameScreen);
+
+const objectA = new RenderObject(100, 100, { x: 100, y: 100 }, "red");
+renderer.renderObjects.push(objectA);
+
+renderer.renderAll();
