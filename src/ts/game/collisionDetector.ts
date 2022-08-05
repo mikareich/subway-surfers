2dd3bb7f89b50345b7a3893cb165830906d5afb5
Käkäsh <marco.zillgen23@gmail.com>
2022-08-05 23:21:43 +0200

    collisionDetector done

---
+++ src/ts/game/collisionDetector.ts
@@ -0,0 +1,21 @@
+interface position {
+  x: number
+  y: number
+}
+interface GameObject {
+  position: position
+  width: number
+  height: number
+}
+
+export default function collisionDetector (
+  gameObject1: GameObject,
+  gameObject2: GameObject
+): boolean {
+  return (
+    gameObject1.position.x < gameObject2.position.x + gameObject2.width &&
+    gameObject1.position.x + gameObject1.width > gameObject2.position.x &&
+    gameObject1.position.y < gameObject2.position.y + gameObject2.height &&
+    gameObject1.position.y + gameObject1.height > gameObject2.position.y
+  )
+}
